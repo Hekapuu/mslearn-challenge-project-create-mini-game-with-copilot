82259beedacd47d46f1e8e616cbf9168d85d37c0
juanencode <134582373+Hekapuu@users.noreply.github.com>
2023-11-26 18:50:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,3 +9,47 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return app.send_static_file("index.html")
+
+
+import random
+
+def jugar_piedra_papel_tijera():
+    opciones = ["piedra", "papel", "tijera"]
+    puntaje = {"jugador": 0, "oponente": 0}
+
+    def resultado_ronda(jugador, oponente):
+        if jugador == oponente:
+            return "Empate"
+        elif (
+            (jugador == "piedra" and oponente == "tijera")
+            or (jugador == "tijera" and oponente == "papel")
+            or (jugador == "papel" and oponente == "piedra")
+        ):
+            puntaje["jugador"] += 1
+            return "¡Ganaste!"
+        else:
+            puntaje["oponente"] += 1
+            return "¡Perdiste!"
+
+    while True:
+        print("\nElige: piedra, papel o tijera. Para salir, escribe 'salir'.")
+        eleccion_jugador = input().lower()
+
+        if eleccion_jugador == "salir":
+            break
+        elif eleccion_jugador not in opciones:
+            print("Opción no válida. Por favor, elige piedra, papel o tijera.")
+            continue
+
+        eleccion_oponente = random.choice(opciones)
+        print(f"\nOponente elige: {eleccion_oponente}")
+
+        resultado = resultado_ronda(eleccion_jugador, eleccion_oponente)
+        print(f"\n{resultado}")
+        print(f"Puntaje: Jugador {puntaje['jugador']} - {puntaje['oponente']} Oponente")
+
+    print("\nJuego terminado. ¡Hasta luego!")
+
+# Inicia el minijuego
+jugar_piedra_papel_tijera()
+
